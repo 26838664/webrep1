58d133c1414ea327f913a873fd8fce611070ad7c
Цыганков Александр Григорьевич <s8300@psta.ru>
2021-10-04 15:09:47 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+      <head>
+	<meta charset="utf-8">
+          <title>Моя страничка</title>
+         
+     </head>
+     <body>
+          <div class="mydiv1">
+           <?php 
+               echo "Квадрат числа 5 равен ";
+               echo  5*5;
+            ?>
+    </div>
+  </body>
+</html>
